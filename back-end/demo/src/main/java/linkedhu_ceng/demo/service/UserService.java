6c2a3b553f2989b221faf89a0d6a695b215a1c5b
b21996126 <56737553+b21996126@users.noreply.github.com>
2022-04-14 13:04:15 +0300

    Update UserService.java

--- back-end/demo/src/main/java/linkedhu_ceng/demo/service/UserService.java
+++ back-end/demo/src/main/java/linkedhu_ceng/demo/service/UserService.java
@@ -1,16 +1,14 @@
 package linkedhu_ceng.demo.service;
 
-
-import linkedhu_ceng.demo.dto.UserDto;
+import linkedhu_ceng.demo.dto.SignUpDto;
 import linkedhu_ceng.demo.entity.User;
 import linkedhu_ceng.demo.repository.UserRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,25 +18,7 @@ public class UserService {
     UserRepository userRepository;
 
     @Autowired
-    ModelMapper modelMapper;
-
-    public void saveUser(UserDto userDto) {
-        Optional<User> optionalUser = userRepository.findByEmail(userDto.getEmail());
-        if (!optionalUser.isPresent()) {
-            // insert new user
-            User user = modelMapper.map(userDto, User.class);
-            userRepository.save(user);
-        }
-    }
-    /***
-    public User getUser(String email) {
-        Optional<User> optionalUser = userRepository.findByEmail(email);
-        if (optionalUser.isPresent()) {
-            return optionalUser.get();
-        }
-        return null;
-    }***/
-
+    PasswordEncoder passwordEncoder;
 
     public User getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -47,7 +27,32 @@ public class UserService {
         return optionalUser.get();
     }
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public User updateUser(SignUpDto signUpDto){
+        User user = getUser();
+
+        if(signUpDto.getName() != null){
+            user.setName(signUpDto.getName());
+        }
+        if(signUpDto.getSurname() != null){
+            user.setSurname(signUpDto.getSurname());
+        }
+        if(signUpDto.getEmail() != null){
+            user.setEmail(signUpDto.getEmail());
+        }
+        if(signUpDto.getPassword() != null){
+            user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+        }
+        if(signUpDto.getPhoneNumber() != null){
+            user.setPhoneNumber(signUpDto.getPhoneNumber());
+        }
+
+        userRepository.save(user);
+        return user;
+    }
+
+    public void deleteUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userId = authentication.getName();
+        userRepository.deleteById(userId);
     }
 }
