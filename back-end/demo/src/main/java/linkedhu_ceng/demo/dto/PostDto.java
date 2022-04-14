d68c3e0302a4b0d7eb6c87815f0a232c44c9d869
b21996126 <56737553+b21996126@users.noreply.github.com>
2022-04-14 14:14:51 +0300

    Update PostDto.java

--- back-end/demo/src/main/java/linkedhu_ceng/demo/dto/PostDto.java
+++ back-end/demo/src/main/java/linkedhu_ceng/demo/dto/PostDto.java
@@ -13,6 +13,14 @@ public class PostDto {
     public void setId(Integer id) {
         this.id = id;
     }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getContent() {
         return content;
