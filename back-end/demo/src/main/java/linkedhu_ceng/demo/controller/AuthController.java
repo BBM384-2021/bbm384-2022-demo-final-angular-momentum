73698d7e33c0bdbc187d03fb7485f447cd023ce9
b21996126 <56737553+b21996126@users.noreply.github.com>
2022-05-08 21:51:58 +0300

    Json Token Added

--- back-end/demo/src/main/java/linkedhu_ceng/demo/controller/AuthController.java
+++ back-end/demo/src/main/java/linkedhu_ceng/demo/controller/AuthController.java
@@ -90,7 +90,31 @@ public class AuthController {
         return model;
     }
 
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public Object createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
+        authenticate(authenticationRequest.getUserId(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUserId());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return new JwtResponse(token);
+
+        //return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String userId, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userId, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+    
+    
     /*@GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
