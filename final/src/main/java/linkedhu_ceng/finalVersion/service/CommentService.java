255584915cdf910e3c629eba3c82f7d32b636547
Enes GÖKCE <56717764+b21946132@users.noreply.github.com>
2022-05-09 00:35:09 +0300

    Update CommentService.java

--- final/src/main/java/linkedhu_ceng/finalVersion/service/CommentService.java
+++ final/src/main/java/linkedhu_ceng/finalVersion/service/CommentService.java
@@ -29,10 +29,11 @@ public class CommentService {
         comment.setText(commentDto.getText());
         comment.setCreatedById(userId);
 
-        if (comment.getId() == null)
-            comment.setCreatedDate(ZonedDateTime.now());
-        else
-            comment.setCreatedDate(commentDto.getCreatedDate());
+        comment.setCreatedDate(ZonedDateTime.now());
+        //if (comment.getId() == null)
+        //comment.setCreatedDate(ZonedDateTime.now());
+        //else
+        //comment.setCreatedDate(commentDto.getCreatedDate());
 
         return commentRepo.save(comment);
     }
@@ -48,5 +49,3 @@ public class CommentService {
 
     }
 }
-
-
