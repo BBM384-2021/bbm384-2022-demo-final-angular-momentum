7c005a50e0df1bf072fbec2390fabc317465b665
b21989712 <56718081+b21989712@users.noreply.github.com>
2022-05-12 19:58:30 +0300

    Add files via upload

---
+++ final/src/main/java/linkedhu_ceng/finalVersion/dto/ProfileDto.java
@@ -0,0 +1,60 @@
+package linkedhu_ceng.finalVersion.dto;
+
+public class ProfileDto {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String job;
+    private String company;
+    private String bio;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
